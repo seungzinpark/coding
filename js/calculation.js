51c27e235a0e26f6662d3a44ac3be28193407336
Seungjin Park <Seungjin@bagseungjin-ui-MacBookAir.local>
2022-04-23 11:25:36 +0900

    #9 js 사칙연산

---
+++ js/calculation.js
@@ -0,0 +1,8 @@
+let num1 = 10;
+let num2 = 5;
+console.log("더하기",num1+num2);
+console.log("빼기",num1-num2);
+console.log("곱하기",num1*num2);
+console.log("나누기",num1/num2);
+console.log("나머지",num1%num2);
+console.log("지수곱",num1**num2);
