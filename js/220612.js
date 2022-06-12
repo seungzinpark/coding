a6f359800f3e1d37dc64cab5ee8952526436ba19
Seungjin Park <Seungjin@bagseungjin-ui-MacBookAir.local>
2022-06-12 17:51:20 +0900

    #20 for반복문

--- js/220612.js
+++ js/220612.js
@@ -1,19 +1,18 @@
-const aaa = ['melon', 'banana', 'lemon', 'tomato', 'grape'];
-function searchMelon(fruits){
-let flag = false;
-    for(let i=0; i<fruits.length; i++){
-        console.log(fruits[i]);
-    }
-    // if(fruits.includes('melon')){
-    //     console.log("멜론 있습니다.");
-    // } else{
-    //     console.log("멜론 없습니다.");       
-    // }
-    let hasMelon = fruits.includes('melon') ? "멜론 있습니다" : "멜론 없습니다"
-    console.log(hasMelon);
-    // let 깃발 = flag ? "멜론이 있습니다" : "멜론이 없습니다"
-    // console.log(깃발);
-
-    return fruits.join('-');
+// 배열에 특화된 for (value of arr)
+const arr = [1,2,3,4,5,6]
+for (value of arr){
+    console.log(value);
 }
-console.log(searchMelon(aaa))
+
+// 객체에 특화된 for (key in obj)
+const arr = [1,2,3,4,5,6]
+for (key in arr){
+    console.log(key);
+}
+const person = {
+    name: "SJ",
+    age: 26
+}
+for (key in person){
+    console.log(key);
+}
